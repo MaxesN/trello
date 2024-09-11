2233d053c0e4ba348f4703ef44219f80b05b3820
Maxesn <sharkkizayk@gmail.com>
2024-09-11 17:13:09 +0300

    chore(pinia): install & setup

---
+++ src/app/index.ts
@@ -0,0 +1,11 @@
+import { createApp } from 'vue'
+
+import App from './App.vue'
+import { pinia } from './providers'
+
+import './style/style.css'
+
+const app = createApp(App)
+
+app.use(pinia)
+app.mount('#app')
