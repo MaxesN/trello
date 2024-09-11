58a80da6368e620edef4e4aeb400b4974a0df160
Maxesn <sharkkizayk@gmail.com>
2024-09-11 20:09:57 +0300

    feat: added Task type

---
+++ src/shared/types/models.ts
@@ -0,0 +1,16 @@
+export interface Person {
+  id: string
+  firstName: string
+  lastName: string
+  avatar?: string
+}
+
+export interface Task {
+  id: string
+  name: string
+  description: string
+  status: 'todo' | 'in_progress' | 'done'
+  priority: 'high' | 'medium' | 'low'
+  performers: Person[]
+  responsiblePerson: Person
+}
