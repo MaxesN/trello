3a0e25a2393242fa845acaffa0253cf527b0ec08
Maxesn <sharkkizayk@gmail.com>
2024-09-12 17:33:43 +0300

    fix: style

--- src/shared/ui/TButton/TButton.vue
+++ src/shared/ui/TButton/TButton.vue
@@ -5,7 +5,7 @@ defineProps</* @vue-ignore */ ButtonHTMLAttributes>()
 </script>
 
 <template>
-  <button class="hover:bg-black/10 py-1 px-2 rounded-md transition-all focus:outline-none">
+  <button class="hover:bg-black/10 w-full py-1 px-2 rounded-md transition-all focus:outline-none">
     <slot />
   </button>
 </template>
