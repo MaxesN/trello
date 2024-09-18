8da52e4fca69f5a9fb983dcf7f1ea23c9b2e48f4
MaxesN <sharkkizayk@gmail.com>
2024-09-19 01:45:55 +0300

    feat: added models

---
+++ src/shared/model/user.ts
@@ -0,0 +1,24 @@
+import type { User } from '@/shared/types'
+
+export const users: User[] = [
+  {
+    firstName: 'Oleg',
+    lastName: 'Gasw',
+    id: 'c7d19ea7-6252-4b54-af34-c2fd5d442aeb',
+  },
+  {
+    firstName: 'Max',
+    lastName: 'Trawn',
+    id: 'c7d22ea7-6252-4b54-af11-c2fd5d442aeb',
+  },
+  {
+    firstName: 'Yuri',
+    lastName: 'Braw',
+    id: 'c7d222a7-62g2-4b54-af11-c2fd5d442aeb',
+  },
+  {
+    firstName: 'Taras',
+    lastName: 'Logic',
+    id: 'c7d22ea7-62w2-4b54-a222-c2wwdwdd2aeb',
+  },
+]
