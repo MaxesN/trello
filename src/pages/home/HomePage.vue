3e014ce58b031d468f658c5d673cbf1af944a314
MaxesN <sharkkizayk@gmail.com>
2024-09-19 01:43:37 +0300

    feat: added Home page

---
+++ src/pages/home/HomePage.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { TaskList } from '@/feature/task-list'
+import { useTask } from '@/feature/task-list/stores/task'
+import draggable from 'vuedraggable'
+
+const { taskLists } = useTask()
+</script>
+
+<template>
+  <div class="flex gap-4 items-start overflow-x-auto">
+    <draggable
+      :list="taskLists"
+      item-key="id"
+      class="flex gap-4 justify-start w-full list"
+    >
+      <template #item="{ element }">
+        <div
+          class="w-full"
+        >
+          <TaskList v-bind="element" />
+        </div>
+      </template>
+    </draggable>
+  </div>
+</template>
